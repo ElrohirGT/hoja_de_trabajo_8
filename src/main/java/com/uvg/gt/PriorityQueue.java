f36b3e804f6f6ed45bde76236016dfa8fd67622a
ElrohirGT <elrohirgt@gmail.com>
2023-03-24 12:32:58 -0600

    feat: Added a basic impl

---
+++ src/main/java/com/uvg/gt/PriorityQueue.java
@@ -0,0 +1,23 @@
+package com.uvg.gt;
+
+public class PriorityQueue<T> implements IPriorityQueue<T> {
+
+    @Override
+    public T dequeue() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'dequeue'");
+    }
+
+    @Override
+    public void insert(T value, int priority) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insert'");
+    }
+
+    @Override
+    public T peek() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'peek'");
+    }
+
+}
