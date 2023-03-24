0c88ce72a4c9423e578fbec4bfdfe6a707dd91bd
ElrohirGT <elrohirgt@gmail.com>
2023-03-24 12:18:46 -0600

    feat: Updated the interface to the project requisites

--- src/main/java/com/uvg/gt/IPriorityQueue.java
+++ src/main/java/com/uvg/gt/IPriorityQueue.java
@@ -10,9 +10,9 @@ public interface IPriorityQueue<T> {
      * Inserts a value into the priority queue with the given priority.
      * 
      * @param value    the value to insert.
-     * @param priority the priority level.
+     * @param priority the priority level. Lower means more priority.
      */
-    public void insert(T value, Priority priority);
+    public void insert(T value, int priority);
 
     /**
      * @return the element that get would return.
--- src/main/java/com/uvg/gt/Priority.java
+++
@@ -1,7 +0,0 @@
-package com.uvg.gt;
-
-public enum Priority {
-    LOW,
-    MEDIUM,
-    HIGH
-}
